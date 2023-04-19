1575e574c97d6f5ce931135e2af017e671c8f7fa
Tumul Prakash <111174055+tumul3131@users.noreply.github.com>
2023-04-19 13:48:02 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,36 @@
+var sub_text="Form successfully submitted !";
+function changebutton(){
+    alert(sub_text);
+    location.href='project_backup.php';
+}
+
+function verifyPswd(){
+    var pswd=prompt("Enter admin password :");
+    if(pswd==="fullstack22*"){
+        window.alert("Password correct !");
+        window.location='add.html';
+    }
+    else{
+        window.alert("Password incorrect !");
+        window.location='form1.html';
+    }
+}
+function loginbutton(){
+    location.href='login_form.php';
+}
+
+$(document).ready(
+        function(){
+            $('#firstname').change(
+                function(){
+                    if($('#firstname').val().length < 5 ){
+                        //$('#firstname').css("background-color","red");
+                        $('#f_name_error').text('*First name must not be smaller than 5');
+                    }
+                    else{
+                        $('#f_name_error').text('');
+                    }
+                }
+            )
+        }
+)
